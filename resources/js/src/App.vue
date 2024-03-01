8c8c2848a4d05d9323b0ac445136c596a4c8ec6b
ErvinDeveloper <ervin.developer@yandex.ru>
2024-03-01 17:17:47 +0300

    added vue

---
+++ resources/js/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+<h1>{{ varName }}</h1>
+</template>
+
+<script>
+export default {
+    name: "App",
+    setup() {
+        return {
+            varName: 'Test'
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
